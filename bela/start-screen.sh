6e6111539da3e013867d51cf6fbe78ea5a44903d
Brian Fay <ovaltinevortex@gmail.com>
2020-07-11 13:41:15 -0400

    load default patch on screen sesh startup

--- bela/start-screen.sh
+++ bela/start-screen.sh
@@ -8,4 +8,4 @@ screen -S baliset -X screen -t ui_server
 screen -S baliset -X screen -t shell
 screen -S baliset -p 0 -X stuff './build/baliset'
 screen -S baliset -p 1 -X stuff 'cd ui/server'
-screen -S baliset -p 1 -X stuff 'node ./app.js'
+screen -S baliset -p 1 -X stuff 'node ./app.js default.json'
